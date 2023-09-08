c9f2ebebb93b3a8075b9a5ce581c0471e63960c4
Alexander Gostilo <alexandergostilo@gmail.com>
2023-09-09 02:19:23 +0300

    add objects

---
+++ js/obj.js
@@ -0,0 +1,13 @@
+let userObj = {
+    name: "alexa",
+    age: "undefined",
+    sex: "undefined",
+    "net worth": 5000,
+};
+
+let keySex = "sex";
+
+alert(userObj.name);
+alert(userObj["age"]);
+alert(userObj[keySex]);
+alert(userObj["net worth"]);
